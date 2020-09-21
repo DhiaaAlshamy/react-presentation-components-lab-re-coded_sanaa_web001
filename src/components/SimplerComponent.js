cdcb20deb3321f9243e031241d06c776e204c859
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 13:56:32 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,18 @@
 // Code SimplerComponent Here
+import React from 'react'
+extract default const SimplerComponent extends Component {
+
+
+handleClick = () => {
+  // Probably do some work to update the state
+  this.setState(prevState,{
+    mode:prevState==='happy'?'Sad':'happy'
+  })
+}
+    return(
+      <div>
+        {this.state.mode}
+      </div>
+    )
+  
+}
