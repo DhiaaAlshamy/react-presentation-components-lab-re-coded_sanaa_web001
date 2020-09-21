98d6f05046984b9386d3f1ac5a320df106d8410d
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 13:57:53 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,17 +1,11 @@
 // Code SimplerComponent Here
 import React from 'react'
-extract default const SimplerComponent extends Component {
+extract default function SimplerComponent {
 
 
-handleClick = () => {
-  // Probably do some work to update the state
-  this.setState(prevState,{
-    mode:prevState==='happy'?'Sad':'happy'
-  })
-}
     return(
       <div>
-        {this.state.mode}
+        "I am just happy"
       </div>
     )
 
